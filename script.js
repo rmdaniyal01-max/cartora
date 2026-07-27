1359f81c3b8b07e24bdc059e7af626132c421474
rmdaniyal01-max <r.m.daniyal01@gmail.com>
2026-07-27 19:02:40 +0500

    move to top button

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 let menuButton = document.getElementById("menu-button");
 let navLinks = document.getElementById("nav-links");
+let topButton = document.getElementById("top-button");
 
-menuButton.addEventListener("click", function(){
-    navLinks.classList.toggle("show")
+menuButton.addEventListener("click", () => navLinks.classList.toggle("show"))
+
+addEventListener("scroll", () => {
+    if(window.scrollY >= 300){
+        topButton.classList.add("moveToTop");
+        console.log("added")
+    }
+    else{
+        topButton.classList.remove("moveToTop");
+        console.log("removed")
+    }
+})
+
+topButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
 })
