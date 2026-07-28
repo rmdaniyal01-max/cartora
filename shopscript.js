87c2efc3f36f2327d6e49a5a2c18889ab9c157fa
rmdaniyal01-max <r.m.daniyal01@gmail.com>
2026-07-28 21:09:07 +0500

    shop banner

---
+++ shopscript.js
@@ -0,0 +1,4 @@
+let menuButton = document.getElementById("menu-button");
+let navLinks = document.getElementById("nav-links");
+
+menuButton.addEventListener("click", () => navLinks.classList.toggle("show"));
